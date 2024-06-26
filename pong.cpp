92551be959b06585a2289086c0d30c81660dd7db
Saif Garasia <saifgarasia_gmail.com>
2024-06-26 15:42:30 -0500

    made the screen

--- pong.cpp
+++ pong.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <ncurses.h>
+#include <cstdlib>
+
+int window_width = 93;
+int window_height = 21;
+
+int setupwindow()
+{
+	WINDOW *win = newwin(window_height,window_width,1,10);
+	refresh();
+	box(win, 0, 0);
+	mvwprintw(win,10,10,"|                                                                                                              |                                                                                                              |");
+	wrefresh(win);
+	return 0;
+}
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	initscr();
+	setupwindow();
+	getch();
+	endwin();
 	return 0;
 }
